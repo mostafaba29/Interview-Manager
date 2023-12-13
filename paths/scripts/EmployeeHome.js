3b9628f592cbadc0b234900170aee004b1eaa28c
mostafaba29 <mostafa.ba.zaki@gmail.com>
2023-12-13 19:52:43 +0200

    added a probable fix to  an error

--- paths/scripts/EmployeeHome.js
+++ paths/scripts/EmployeeHome.js
@@ -4,17 +4,26 @@ CreateAppointment.addEventListener('click', ()=>{
     let appointmentTable = document.getElementById('appointmentTable');
     let nRow = document.createElement('tr');
     appointmentTable.appendChild(nRow);
-    for(let i=0;i<5;i++){
+    for(let i=0;i<4;i++){
         x = document.createElement('td');
         x.textContent = prompt('typesomeinput','defaultValue');
         nRow.appendChild(x)
     }
+    let cells = nRow.getElementsByTagName('td');
+
+    let data = {
+        MeetingTime:cells[0].textContent,
+        Client:cells[1].textContent,
+        Discription:cells[2].textContent,
+        ManagerName:cells[3].textContent
+    };
+
     fetch('http://localhost:8000/create',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(nRow), 
+        body: JSON.stringify(data), 
     })
     .then(response => response.json())
     .then(data =>{
