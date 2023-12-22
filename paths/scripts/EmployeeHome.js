1e7163cfff7b3b361ab37ef9530a69cd9563142a
mostafaba29 <mostafa.ba.zaki@gmail.com>
2023-12-22 21:42:45 +0200

    added some icons and a form to submit meetings

--- paths/scripts/EmployeeHome.js
+++ paths/scripts/EmployeeHome.js
@@ -1,6 +1,110 @@
-const CreateAppointment = document.getElementsByClassName('Create-appointment-btn')[0];
+
 const logoutBtn = document.getElementById('logoutBtn');
-CreateAppointment.addEventListener('click', ()=>{
+const subForm = document.getElementById('formContainer');
+const subBtn = document.getElementById('Create-appointment-btn')
+/*document.addEventListener("DOMContentLoaded",()=>{
+    subForm.classList.toggle('hidden');
+    subForm.classList.add('invisible');
+    subBtn.addEventListener('click',()=>{
+        subForm.classList.toggle('hidden');
+        document.body.classList.togglte('blur-sm');
+    });
+    const myForm =document.getElementById('myForm');
+    myForm.addEventListener('submit',function(event){
+        event.preventDefault();
+        subForm.classList.add('hidden');
+        document.body.classList.toggle('blur-sm');
+    })
+})*/
+
+subBtn.addEventListener('click',()=>{
+    //document.body.classList.add('blur-sm');
+    const formContainer = document.createElement('div');
+    formContainer.id ='formContainer';
+    formContainer.classList.add('flex','fixed','top-0','left-0','w-full','h-full','items-center','justify-center','z-50');
+    const formContent = document.createElement('div');
+    formContent.classList.add('bg-white','p-8','rounded','shadow-md');
+    formContent.innerHTML=`
+    <h2 class="text-2xl font-bold mb-4">Create Appointment</h2>
+          <form id="createAppointmentForm">
+            <!-- Time input -->
+            <label for="timeInput" class="block mb-2">Time:</label>
+            <input type="time" id="timeInput" name="time" class="w-full border p-2 mb-4">
+
+            <!-- Name input -->
+            <label for="nameInput" class="block mb-2">Name:</label>
+            <input type="text" id="nameInput" name="name" class="w-full border p-2 mb-4">
+
+            <!-- Reason input -->
+            <label for="reasonInput" class="block mb-2">Reason:</label>
+            <textarea id="reasonInput" name="reason" rows="4" class="w-full border p-2 mb-4"></textarea>
+            <!-- Mangar Name -->
+            <label for="managerName" class="block mb-2">Manager Name:</label>
+            <input type="text" id ="managerName" name="managerName" class="w-full border p-2 mb-4">
+            <!-- Submit button -->
+            <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded">Submit</button>
+          </form>
+    `;
+    formContainer.appendChild(formContent);
+    document.body.appendChild(formContainer);
+    //formContainer.classList.remove()
+    const createAppointmentForm = document.getElementById("createAppointmentForm");
+        createAppointmentForm.addEventListener("submit", async function (event) {
+          event.preventDefault();
+          const formData = new FormData(createAppointmentForm);
+          const time = formData.get("time");
+          const name = formData.get("name");
+          const reason = formData.get("reason");
+          const mName =formData.get("managerName");
+          let appointmentData = {
+            Meeting_Time:time,
+            client:name,
+            Description:reason,
+            Manager_Name:mName
+          }
+            fetch('http://localhost:3000/auth/create',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(appointmentData), 
+            })
+            .then(response => response.json())
+            .then(data =>{
+                console.log(data);
+            })
+            .catch(error => {
+                console.log('error creating appointment',error);
+            })
+          formContainer.remove();
+
+          try {
+            const response = await fetch('http://localhost:3000/auth/allappointments');
+            const data = await response.json();
+            renderTable(data);
+          } catch (error) {
+            console.error('Error', error);
+          }
+        });
+    })
+    function renderTable(data){
+        const tableBody = document.querySelector('#appointmentTable');
+        tableBody.innerHTML = '';
+        if (data && data.length >0){
+        data.slice(0, 20).forEach(row => {
+          const newRow = document.createElement('tr');
+          newRow.innerHTML = `
+              <td>${row.meeting_time}</td>
+              <td>${row.client}</td>
+              <td>${row.description}</td>
+              <td>${row.status}</td>
+              <td>${row.manager_name}</td>
+          `;
+          tableBody.appendChild(newRow);
+      });
+    }
+  }
+/*CreateAppointment.addEventListener('click', ()=>{
     let appointmentTable = document.getElementById('appointmentTable');
     let nRow = document.createElement('tr');
     appointmentTable.appendChild(nRow);
@@ -33,7 +137,7 @@ CreateAppointment.addEventListener('click', ()=>{
         console.log('error creating appointment',error);
     })
 
-})
+})*/
 
 logoutBtn.addEventListener('click',()=>{
     fetch('http://localhost:3000/auth/logout',{
