cd59e301a773c2106f8321a537e05fa0d39ea668
Mostafa Elshahawy <mostafaelshahawy67@gmail.com>
2023-12-27 23:07:11 +0200

    used jwt

--- handlers/filterationControllers.go
+++ handlers/filterationControllers.go
@@ -1,38 +1,36 @@
 package handlers
 
 import (
-	"fmt"
 	"mostafaba29/intialization"
-	"mostafaba29/middleware"
 	"mostafaba29/models"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetCurrentUser(c *fiber.Ctx) error {
-	sess, err := middleware.Store.Get(c)
-	if err != nil {
-		c.JSON("failed to get user from session")
-	}
-	fmt.Println(sess)
+// func GetCurrentUser(c *fiber.Ctx) error {
+// 	sess, err := middleware.Store.Get(c)
+// 	if err != nil {
+// 		c.JSON("failed to get user from session")
+// 	}
+// 	fmt.Println(sess)
 
-	username := sess.Get(middleware.User)
-	if username == nil {
-		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "not autherized cant get id",
-		})
-	}
+// 	username := sess.Get(middleware.User)
+// 	if username == nil {
+// 		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 			"error": "not autherized cant get id",
+// 		})
+// 	}
 
-	var user models.User
-	intialization.DB.Where("username=?", username).First(&user)
-	if err != nil {
-		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "not autherized something is wrong",
-		})
-	}
+// 	var user models.User
+// 	intialization.DB.Where("username=?", username).First(&user)
+// 	if err != nil {
+// 		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 			"error": "not autherized something is wrong",
+// 		})
+// 	}
 
-	return c.JSON(user)
-}
+// 	return c.JSON(user)
+// }
 
 func ShowAppointments(c *fiber.Ctx) error {
 	var appointments []models.Appointment
@@ -40,56 +38,56 @@ func ShowAppointments(c *fiber.Ctx) error {
 	return c.JSON(appointments)
 }
 
-func ShowManagerAppointments(c *fiber.Ctx) error {
-	//user := GetCurrentUser(c)
-	sess, err := middleware.Store.Get(c)
-	if err != nil {
-		c.JSON("failed to get user from session")
-	}
-	fmt.Println(sess)
-	username := sess.Get(middleware.User)
-	if username == nil {
-		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "not autherized cant get id",
-		})
-	}
+// func ShowManagerAppointments(c *fiber.Ctx) error {
+//user := GetCurrentUser(c)
+// sess, err := middleware.Store.Get(c)
+// if err != nil {
+// 	c.JSON("failed to get user from session")
+// }
+// fmt.Println(sess)
+// username := sess.Get(middleware.User)
+// if username == nil {
+// 	c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 		"error": "not autherized cant get id",
+// 	})
+// }
 
-	// var user models.User
-	// manager_name := intialization.DB.Where("username=?", username).First(&user)
-	// if err != nil {
-	// 	c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-	// 		"error": "not autherized something is wrong",
-	// 	})
-	// }
-	fmt.Println(username)
+// var user models.User
+// manager_name := intialization.DB.Where("username=?", username).First(&user)
+// if err != nil {
+// 	c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+// 		"error": "not autherized something is wrong",
+// 	})
+// }
+//fmt.Println(username)
 
-	var appointments []models.Appointment
-	// managerName := user
-	//fmt.Println(managerName)
-	if err := intialization.DB.Where("manager_name = ?", username).Find(&appointments).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-	}
+// var appointments []models.Appointment
+// // managerName := user
+// //fmt.Println(managerName)
+// if err := intialization.DB.Where("manager_name = ?", username).Find(&appointments).Error; err != nil {
+// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+// }
 
-	return c.JSON(appointments)
-}
+// return c.JSON(appointments)
+//}
 
-func ShowApprovedAppointments(c *fiber.Ctx) error {
-	var approvedAppointments []models.Appointment
-	if err := intialization.DB.Where("status=?", "Confirmed").First(&approvedAppointments); err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"massege": "no approved appointments found",
-		})
-	}
-	return c.JSON(approvedAppointments)
-}
+// func ShowApprovedAppointments(c *fiber.Ctx) error {
+// 	var approvedAppointments []models.Appointment
+// 	if err := intialization.DB.Where("status=?", "Confirmed").First(&approvedAppointments); err != nil {
+// 		return c.Status(400).JSON(fiber.Map{
+// 			"massege": "no approved appointments found",
+// 		})
+// 	}
+// 	return c.JSON(approvedAppointments)
+// }
 
-func ShowCanceledAppointments(c *fiber.Ctx) error {
-	var canceledAppointments []models.Appointment
-	if err := intialization.DB.Where("status=?", "Declined").First(&canceledAppointments); err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"massege": "no canceled appointments found",
-		})
+// func ShowCanceledAppointments(c *fiber.Ctx) error {
+// 	var canceledAppointments []models.Appointment
+// 	if err := intialization.DB.Where("status=?", "Declined").First(&canceledAppointments); err != nil {
+// 		return c.Status(400).JSON(fiber.Map{
+// 			"massege": "no canceled appointments found",
+// 		})
 
-	}
-	return c.JSON(canceledAppointments)
-}
+// 	}
+// 	return c.JSON(canceledAppointments)
+// }
--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func init() {
 func main() {
 	app := fiber.New()
 	app.Use(middleware.CorsMiddleware())
-	app.Use(middleware.AuthMiddleware())
+	//app.Use(middleware.AuthMiddleware())
 
 	routes.SetupRoutes(app)
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":5000"))
 }
